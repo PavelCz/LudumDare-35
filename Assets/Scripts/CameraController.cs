993d4043cd0e65b8d2c54c55b7a26d049ea6c070
Pavel Czempin <pavel.czempin@in.tum.de>
2016-04-16 17:38:07 +0200

    added camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.position = this.player.transform.position + new Vector3(0, 0,-1);
+	}
+}
