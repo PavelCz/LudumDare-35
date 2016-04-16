34d211d76c1092a5b7b15c4fdd52fa44c2bc3347
Pavel Czempin <pavel.czempin@in.tum.de>
2016-04-16 18:28:21 +0200

    added movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		int horizontal = 0;
+		int vertical = 0;
+
+		bool n = Input.GetKey (KeyCode.W);
+		bool s = Input.GetKey (KeyCode.S);
+		bool w = Input.GetKey(KeyCode.A);
+		bool e = Input.GetKey(KeyCode.D);
+
+
+
+		if (n) {
+			vertical++;
+		}
+		if (s) {
+			vertical--;
+		}
+		if(w) {
+			horizontal--;
+		}
+		if(e) {
+			horizontal++;
+		}
+
+		Vector2 movement = new Vector2 (horizontal, vertical);
+		movement.Normalize ();
+		movement.Scale (new Vector2(speed * Time.deltaTime, speed* Time.deltaTime));
+
+		this.gameObject.transform.Translate (movement);
+	}
+}
