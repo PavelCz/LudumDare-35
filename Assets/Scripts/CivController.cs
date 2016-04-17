32dce43e81b0beaa5c21053363eec49d31a97073
Pavel Czempin <pavel.czempin@in.tum.de>
2016-04-17 20:04:48 +0200

    added random movement to civilians

--- Assets/Scripts/CivController.cs
+++ Assets/Scripts/CivController.cs
@@ -3,14 +3,39 @@ using System.Collections;
 
 public class CivController : MonoBehaviour {
 
+	private float waitTime;
+	private float counter;
+	private Vector3 target;
+
+	public float speed;
+
 	// Use this for initialization
 	void Start () {
-	
+		counter = waitTime;
+		waitTime = Random.Range (5, 20);
+		this.target = FindRandomPointRelative ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		this.counter -= Time.deltaTime;
+
+		if (counter <= 0) {
+			counter = waitTime;
+			target = FindRandomPointRelative ();
+
+		}
+		Vector3 movement = (this.target - this.transform.position);
+		movement.Normalize ();
+		float step = speed * Time.deltaTime;
+		movement.Scale (new Vector2 (step, step));
+		this.transform.position += movement;
+	}
+
+	Vector2 FindRandomPointRelative() {
+		float x = Random.value * 10 - 5;
+		float y = Random.value * 10 - 5;
+		return transform.position + new Vector3(x,y,0);
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
