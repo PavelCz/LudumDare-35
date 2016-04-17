5f774ff506f4e6b6e500256648c7b1bdbecbba18
Pavel Czempin <pavel.czempin@in.tum.de>
2016-04-17 14:45:08 +0200

    added different movement modes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public float mechSpeed;
+
+	public float carSpeed;
 
 	public GameObject mech;
 	public GameObject car;
@@ -16,54 +18,70 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () { 
 		float delta = Time.deltaTime;
 
 		this.UpdateMode();
-		
-		int horizontal = 0;
-		int vertical = 0;
 
 		bool up = Input.GetKey(KeyCode.W);
 		bool down = Input.GetKey(KeyCode.S);
 		bool left = Input.GetKey(KeyCode.A);
 		bool right = Input.GetKey(KeyCode.D);
 
-		if (up) {
-			vertical++;
+		if (this.mode == 1) {
+			this.MechMovement (delta, up, down, left, right);
+		} else {
+			this.CarMovement (delta, up, down, left, right);
 		}
-		if (down) {
-			vertical--;
-		}
-		if(left) {
-			horizontal--;
-		}
-		if(right) {
-			horizontal++;
-		}
-		float distance = speed * delta;
-
-		Vector2 movement = new Vector2 (horizontal, vertical);
-		movement.Normalize ();
-		movement.Scale (new Vector2(distance, distance));
-
-		this.activeGameObject.transform.Translate (movement);
 	}
 
 	void UpdateMode() {
-		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+		if (Input.GetKeyDown (KeyCode.E)) {
 			this.mode = 0;
 			this.car.SetActive (true);
 			this.mech.SetActive (false);
 			this.car.transform.position = this.activeGameObject.transform.position;
 			this.activeGameObject = this.car;
 
-		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+		} else if (Input.GetKeyDown (KeyCode.Q)) {
 			this.mode = 1;
 			this.car.SetActive (false);
 			this.mech.SetActive (true);
 			this.mech.transform.position = this.activeGameObject.transform.position;
 			this.activeGameObject = this.mech;
 		}
+	}
+
+	void CarMovement(float delta, bool up, bool down, bool left, bool right) {
+		if (up) {
+			this.activeGameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (carSpeed * delta, 0));
+		}
+	}
+
+	void MechMovement(float delta, bool up, bool down, bool left, bool right) {
+		int horizontal = 0;
+		int vertical = 0;
+
+
+		if (Input.GetKey(KeyCode.W)) {
+			Debug.Log ("W");
+			vertical++;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			vertical--;
+		}
+		if(Input.GetKey(KeyCode.A)) {
+			horizontal--;
+		}
+		if(Input.GetKey(KeyCode.D)) {
+			horizontal++;
+		}
+		float distance = mechSpeed * delta;
+
+		Vector2 movement = new Vector2 (horizontal, vertical);
+		movement.Normalize ();
+		movement.Scale (new Vector2(distance, distance));
+
+		this.activeGameObject.transform.Translate (movement);
 	}
 }
