e6ca6acde9c52b15b10a1efd09d4c8ac2e6785cc
Pavel Czempin <pavel.czempin@in.tum.de>
2016-04-18 18:45:53 +0200

    Slider now rises

---
+++ Assets/Scripts/SliderController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour {
+	public float risingSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.GetComponent<Slider> ().value += risingSpeed * Time.deltaTime;
+	}
+}
